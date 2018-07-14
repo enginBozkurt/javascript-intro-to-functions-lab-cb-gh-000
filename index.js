66b7509b172f21d89568d6c5e560a139e81aa489
Engin Bozkurt <engin.bozkurt034@gmail.com>
2018-07-14 15:15:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,3 +10,18 @@ function logShout(string) {
   console.log(string.toUpperCase())
 }
 
+function logWhisper(string){
+  console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma(string){
+  if (string.toLowerCase() === string) {
+    return "I can\'t hear you!"
+  }
+  else if (string.toUpperCase() === string) {
+    return "YES INDEED!"
+  }
+  else if (string === "I love you, Grandma.") {
+    return "I love you, too."
+  }
+}
